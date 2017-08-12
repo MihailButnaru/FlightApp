bd2a9366df0cca130e98cd10381c7dce146a6cdb
MIHAIL BUTNARU <butnarum@hotmail.com>
2017-08-12 16:38:31 +0100

    Update
    Update Changes

--- FlightMe/MenuOnController.swift
+++ FlightMe/MenuOnController.swift
@@ -13,35 +13,12 @@ class MenuOnController: UIViewController {
     
     @IBOutlet var backGroundMenu: UIImageView!
     
-    let transition = CircularTransition()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        aboutButton.layer.cornerRadius = aboutButton.frame.size.width / 2
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        let secondVC = segue.destination as! buttonViewController
-//        secondVC.transitioningDelegate = self.aboutButton
-        secondVC.modalPresentationStyle = .custom
-    }
-    
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.transitionMode = .present
-        transition.startingPoint = aboutButton.center
-        transition.circleColor = aboutButton.backgroundColor!
-        
-        return transition
-    }
-    
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transition.transitionMode = .dismiss
-        transition.startingPoint = aboutButton.center
-        transition.circleColor = aboutButton.backgroundColor!
-        
-        return transition
-    }
-    override func didReceiveMemoryWarning() {
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
