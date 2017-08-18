9a38b79ffd775b75ddb3beb5cbd6956bc8d56bde
MIHAIL BUTNARU <butnarum@hotmail.com>
2017-08-18 14:11:39 +0100

    Flight Push
    Flight push

---
+++ FlightMe/lutonController.swift
@@ -0,0 +1,20 @@
+//
+//  lutonController.swift
+//  FlightMe
+//
+//  Created by MIHAIL BUTNARU on 12/08/2017.
+//  Copyright © 2017 MIHAIL BUTNARU. All rights reserved.
+//
+
+import UIKit
+
+class lutonController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
